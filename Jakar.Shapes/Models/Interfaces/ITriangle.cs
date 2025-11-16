a1c8061b067bb07544fb3d196664ed63b2203d32
Tyler Stegmaier <jakar510@gmail.com>
2025-11-16 17:51:53 -0600

    Add project files.

---
+++ Jakar.Shapes/Models/Interfaces/ITriangle.cs
@@ -0,0 +1,16 @@
+﻿// Jakar.Extensions :: Jakar.Shapes
+// 07/14/2025  10:32
+
+namespace Jakar.Shapes.Interfaces;
+
+
+public interface ITriangle<TSelf> : IShape<TSelf>, IShapeLocation
+    where TSelf : struct, ITriangle<TSelf>
+{
+    ReadOnlyPoint A { get; }
+    ReadOnlyPoint B { get; }
+    ReadOnlyPoint C { get; }
+
+
+    public abstract static TSelf Create( ReadOnlyPoint a, ReadOnlyPoint b, ReadOnlyPoint c );
+}
